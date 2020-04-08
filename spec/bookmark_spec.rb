756bc7f7da5d911b4a671174626c60e41c2e36fc
Alex L <macbookpro@macbooks-mbp.home>
2020-04-08 14:56:21 +0100

    refactor - hard code array to access database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,7 +5,7 @@ describe Bookmark do
 
   describe '#all' do
     it 'returns a list of bookmarks' do
-      expect(Bookmark.all).to include "http://makers.tech"
+      expect(Bookmark.all).to include "http://www.twitter.com"
       expect(Bookmark.all).to include "http://www.destroyallsoftware.com"
       expect(Bookmark.all).to include "http://www.google.com"
     end
