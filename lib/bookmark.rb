e638df1c7441e41c68ab5610efe232a59c0561c6
Alex L <macbookpro@macbooks-mbp.home>
2020-04-08 14:30:10 +0100

    changing app.rb to now use Bookmark.all rather than use instance variable

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,9 @@
 class Bookmark
 
-  attr_reader :all
-
-  def initialize
-    @all = ["www.google.co.uk"]
+  def self.all
+    ["http://makers.tech",
+     "http://www.destroyallsoftware.com",
+     "http://www.google.com"
+    ]
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,19 +2,12 @@ require 'bookmark'
 
 describe Bookmark do
   let(:bookmarks) { Bookmark.new }
-  it 'responds to #bookmark' do
-    expect(bookmarks).to respond_to :all
-  end
 
-
-
-  describe '.all' do
+  describe '#all' do
     it 'returns a list of bookmarks' do
-   
-      expect(bookmarks).to include "http://makers.tech"
-      expect(bookmarks).to include "http://www.destroyallsoftware.com"
-      expect(bookmarks).to include "http://www.google.com"
+      expect(Bookmark.all).to include "http://makers.tech"
+      expect(Bookmark.all).to include "http://www.destroyallsoftware.com"
+      expect(Bookmark.all).to include "http://www.google.com"
     end
   end
-
 end
