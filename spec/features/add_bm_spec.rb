a1109cf07b65ddcb7e0021ae748e221365a8df57
Faye Carter <fayecarter333@gmail.com>
2020-04-10 08:31:11 +0100

    Title column added to database, erb updated to enter title field - tests failing

--- spec/features/add_bm_spec.rb
+++ spec/features/add_bm_spec.rb
@@ -3,8 +3,9 @@ feature "add bookmarks" do
   scenario 'we a new bookmark is added' do
     visit '/add_bookmark'
     fill_in 'url', with: 'www.google.co.uk'
+    fill_in 'title', with: 'Test Bookmark'
     click_button 'Submit'
-    expect(page).to have_content 'www.google.co.uk'
+    expect(page).to have_link('Test Bookmark', href: 'www.google.co.uk')
   end
 
 end
