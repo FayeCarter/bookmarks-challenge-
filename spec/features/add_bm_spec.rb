5ac112df6aedc90eba0ef9931b1efce71559f16a
Faye Carter <fayecarter333@gmail.com>
2020-04-10 11:24:49 +0100

    test - url should be tested to be a url - failing

--- spec/features/add_bm_spec.rb
+++ spec/features/add_bm_spec.rb
@@ -8,4 +8,12 @@ feature "add bookmarks" do
     expect(page).to have_link('Test Bookmark', href: 'www.google.co.uk')
   end
 
+  scenario 'The bookmark must be a valid URL' do
+    visit('add_bookmark')
+    fill_in('url', with: 'not a real bookmark')
+    click_button('Submit')
+  
+    expect(page).not_to have_content "not a real bookmark"
+    expect(page).to have_content "You must submit a valid URL."
+  end
 end
