6d005eb1a5595d1904751f00bccaf0d62ae034a0
Faye Carter <fayecarter333@gmail.com>
2020-04-10 11:17:07 +0100

    Bookmark recatored for readability

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -14,14 +14,21 @@ class Bookmark
   def self.all
     result = DatabaseConnection.query('SELECT * FROM bookmarks')
     result.map do |bookmark|
-      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+      Bookmark.new(
+        id: bookmark['id'], 
+        title: bookmark['title'], 
+        url: bookmark['url']
+      )
     end
   end
 
   def self.create(title:, url:)
     result = DatabaseConnection.query("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
-    
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    Bookmark.new(
+      id: result[0]['id'], 
+      title: result[0]['title'], 
+      url: result[0]['url']
+    )
   end
 
   def self.delete(id:)
@@ -30,12 +37,20 @@ class Bookmark
   
   def self.update(id:, url:, title:)
     result = DatabaseConnection.query("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    Bookmark.new(
+      id: result[0]['id'], 
+      title: result[0]['title'], 
+      url: result[0]['url']
+    )
   end
 
   def self.find(id:)
     result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id};")
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    Bookmark.new(
+      id: result[0]['id'], 
+      title: result[0]['title'], 
+      url: result[0]['url']
+    )
   end
 
 end
