16c2b5170227d7503bcaf4ce0b2cda6dba152fb6
Faye Carter <fayecarter333@gmail.com>
2020-04-10 09:35:48 +0100

    test - improve navigation add view bookmarks button - fail

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,6 +1,13 @@
 feature "Homepage to have content" do
+  
   scenario "index loads with welcome message" do
     visit '/'
     expect(page).to have_content "Hello world"
   end
+
+  scenario "with view bookmarks button" do
+    visit '/'
+    expect(page).to have_button "view"
+  end
+
 end
