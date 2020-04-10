03f2ced2af0c9b4b39fa73e5113f05d2313898c8
Faye Carter <fayecarter333@gmail.com>
2020-04-10 08:35:19 +0100

    Bookmark.create test updated to reflect new column - tests failing

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,7 +7,7 @@ describe Bookmark do
   end
 
 
-  describe '#all' do
+  describe '.all' do
     it 'returns a list of bookmarks' do
       Bookmark.create('http://www.makersacademy.com')
       Bookmark.create('http://www.destroyallsoftware.com')
@@ -18,6 +18,15 @@ describe Bookmark do
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
+    end
+  end
+
+  describe '.create' do
+    it 'creates a bookmarks' do
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark').first
+
+      expect(bookmark['url']).to eq 'http://www.testbookmark.com'
+      expect(bookmark['title']).to eq 'Test Bookmark'
     end
   end
 end
