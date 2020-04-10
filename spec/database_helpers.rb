de60f639a174c572260b93eb58afac0f95f92366
Faye Carter <fayecarter333@gmail.com>
2020-04-10 12:41:00 +0100

    persist method corrected

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -1,10 +1,11 @@
 require 'pg'
+require './database_connection_setup'
 
-def persisted_data(id:)
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-  result = connection.query("SELECT * FROM bookmarks WHERE id = #{id};")
-  result.first
-end
+# def persisted_data(id:)
+#   connection = PG.connect(dbname: 'bookmark_manager_test')
+#   result = connection.query("SELECT * FROM bookmarks WHERE id = #{id};")
+#   result.first
+# end
 
 def persisted_data(table:, id:)
   DatabaseConnection.query("SELECT * FROM #{table} WHERE id = '#{id}';")
