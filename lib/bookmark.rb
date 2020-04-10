078760b1dd9a4792bc977bc1c9207306f0586bf2
Faye Carter <fayecarter333@gmail.com>
2020-04-10 11:04:10 +0100

    Bookmark updated with db connection - failing

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'database_connection'
 
 class Bookmark
 
@@ -11,13 +12,7 @@ class Bookmark
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = connection.exec('SELECT * FROM bookmarks')
+    result = DatabaseConnection.query('SELECT * FROM bookmarks')
     result.map do |bookmark|
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
     end
@@ -63,5 +58,5 @@ class Bookmark
     result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
-  
+
 end
