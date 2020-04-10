6461f669ae0d56979b4bfd4f0671201567728deb
Faye Carter <fayecarter333@gmail.com>
2020-04-10 09:11:01 +0100

    database_helper file created

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require_relative 'database_helpers'
 
 describe Bookmark do
   
@@ -27,12 +28,12 @@ describe Bookmark do
   describe '.create' do
     it 'creates a bookmarks' do
       bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
-      persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
+      persisted_data = persisted_data(id: bookmark.id)
 
-    expect(bookmark).to be_a Bookmark
-    expect(bookmark.id).to eq persisted_data.first['id']
-    expect(bookmark.title).to eq 'Test Bookmark'
-    expect(bookmark.url).to eq 'http://www.testbookmark.com'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
     end
   end
 end
